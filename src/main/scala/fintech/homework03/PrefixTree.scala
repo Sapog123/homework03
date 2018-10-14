1bc80fc0a7dd187d8c9711b852d06418f25bffa7
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-10-14 21:11:16 +0500

    Update PrefixTree.scala

--- src/main/scala/fintech/homework03/PrefixTree.scala
+++ src/main/scala/fintech/homework03/PrefixTree.scala
@@ -11,5 +11,42 @@ trait PrefixTree[K, +V] {
   def put[U >: V](path: Seq[K], value: U): PrefixTree[K, U]
 
   def sub(path: Seq[K]): PrefixTree[K, V]
+
   def get: V
+}
+
+class Tree[K, +V](val value: Option[V],val trees: Map[K, Tree[K, V]]) extends PrefixTree[K, V] {
+
+  def put[U >: V](path: Seq[K], value: U): Tree[K, U] = {
+    if (path.isEmpty) new Tree[K, U](Some[U](value),trees)
+    else if (trees.contains(path.head)) {
+      val subTree = trees(path.head).put(path.tail, value)
+      new Tree(this.value,trees + (path.head -> subTree))
+    }
+    else new Tree[K, U]( this.value,trees + (path.head -> put(path.tail, value)),)
+  }
+
+  override def hashCode(): Int = {
+    val prime = 97
+    var hash = 1
+    hash = prime * hash + trees.hashCode()
+    hash = prime * hash + value.hashCode()
+    hash
+  }
+
+  override def equals(a: Any): Boolean = {
+    a match {
+      case tree: Tree[K, V] => tree.trees == trees && tree.value.getOrElse() == value.getOrElse()
+      case _ => false
+    }
+  }
+
+  def sub(path: Seq[K]): Tree[K, V] = {
+    if (path.isEmpty) this
+    else if (trees.contains(path.head))
+      trees(path.head).sub(path.tail)
+    else new Tree[K, V](None,Map.empty)
+  }
+
+  def get: V = value.get
 }
