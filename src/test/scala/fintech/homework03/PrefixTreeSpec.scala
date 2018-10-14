e04c2625c8da5a30438e8090161278d5b41d8444
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-10-14 21:13:00 +0500

    Update PrefixTreeSpec.scala

--- src/test/scala/fintech/homework03/PrefixTreeSpec.scala
+++ src/test/scala/fintech/homework03/PrefixTreeSpec.scala
@@ -3,7 +3,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class PrefixTreeSpec extends FlatSpec with Matchers {
   it should "work well with strings" in {
-    val tree: PrefixTree[Char, Int] = ???
+     val tree: Tree[Char, Int] = new Tree[Char, Int](None, Map.empty)
 
     val with42: PrefixTree[Char, Int] = tree.put("abcd", 42)
     with42.sub("ab").sub("cd").get should be (42)
@@ -13,5 +13,5 @@ class PrefixTreeSpec extends FlatSpec with Matchers {
     withDouble.sub("ab").sub("cde").get should be (13.0)
   }
 
-  it should "???"
+  //it should "???"
 }
